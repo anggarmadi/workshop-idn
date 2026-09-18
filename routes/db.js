d5b9231bdb9b54488f13a103666730ad4027f29a
Sukma Anggarmadi <2111527001_sukma@student.unand.ac.id>
2026-09-18 23:28:07 +0700

    update db

--- routes/db.js
+++ routes/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'SuperSecret123!',
   database: 'testdb'
 });
-connection.connect();
-module.exports = connection;
 
+connection.connect();
+
+module.exports = connection;
