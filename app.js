447c8089ff09a0ebc59b7641891201f2990c1c10
Sukma Anggarmadi <2111527001_sukma@student.unand.ac.id>
2026-09-18 23:29:08 +0700

    update app.js

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/users');
 
 const app = express();
+
+const JWT_SECRET = 'my-super-secret-key-123';
+
 app.use(bodyParser.json());
+
 app.use('/users', userRoutes);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
-
